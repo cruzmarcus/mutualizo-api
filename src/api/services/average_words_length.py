f2985d35a056c02cbf462b0f4c0db629e43f7b63
Marcus Cruz <marcus.v.s.c13@gmail.com>
2023-04-10 22:29:10 -0300

    unit tests average words length

--- src/api/services/average_words_length.py
+++ src/api/services/average_words_length.py
@@ -1,21 +1,5 @@
 import re
 
-PUNCTUATION_MARKS: str = '".,;:!?-()'
-
-
-async def compute_average_words_length_original(sentence: str) -> float:
-    """
-    Service layer for the endpoint /average-words-length.
-    The function processes a
-    """
-    filtered_sentence = "".join(
-        filter(lambda character: character not in PUNCTUATION_MARKS, sentence)
-    )
-
-    words = [word for word in filtered_sentence.split() if word]
-
-    return sum(len(word) for word in words) / len(words)
-
 
 async def compute_average_words_length(sentence: str) -> float:
     words = sentence.split()
--- tests/unit/services/test_average_words_length.py
+++ tests/unit/services/test_average_words_length.py
@@ -6,5 +6,34 @@ class TestReverseIntegers:
         sentence = "Hi all, my name is Tom... I am originally from Brazil."
         result = await compute_average_words_length(sentence)
 
-        assert result == 3.9
+        assert result == 3.5454545454545454
+
+    async def test_compute_average_words_length_one_word(self) -> None:
+        sentence = "Mutualizo!!!"
+        result = await compute_average_words_length(sentence)
+
+        assert result == 9
     
+    async def test_compute_average_words_length_repetitive_word(self) -> None:
+        sentence = "Hello. Hello? Hello! Hello!!!"
+        result = await compute_average_words_length(sentence)
+
+        assert result == 5
+
+    async def test_compute_average_words_length_loren_ipsum(self) -> None:
+        sentence = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+        "Nullam fermentum finibus justo, ac pellentesque metus tincidunt a. "
+        "Quisque vel arcu id lacus euismod malesuada. Vestibulum sed feugiat "
+        "mauris, sed pellentesque diam. Quisque vitae lacus sed nunc auctor "
+        "sodales. Ut bibendum luctus pellentesque. Donec facilisis nulla "
+        "fermentum enim rutrum pellentesque. Nullam varius posuere dolor, "
+        "et aliquam orci tincidunt a. Pellentesque habitant morbi tristique "
+        "senectus et netus et malesuada fames ac turpis egestas. "
+        "Aenean porttitor scelerisque sapien, blandit vulputate nulla laoreet ac. "
+        " Nunc venenatis vestibulum augue, sit amet pellentesque risus efficitur ut. "
+        "Aenean eu elit luctus, cursus elit ut, tincidunt ligula. Etiam "
+        "interdum lorem vel urna blandit lobortis."
+
+        result = await compute_average_words_length(sentence)
+
+        assert result == 5.875
